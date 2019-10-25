4553ad07ce6e35b8c6aa35aaf21084d63f5b0d17
wlittle-rabbit <591276288@qq.com>
2019-10-25 22:09:40 +0800

    fix

--- linklist.c
+++ linklist.c
@@ -1,28 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct xListitem{
+struct Listitem{
 	int value;
 	struct xListitem *pre;
 	struct xListitem *next;
-}
-
+}; 
+typedef struct Listitem ListItem_t;
 struct List{
 	int number;
-	Listitem *pindex;
-}
-void Listitem_init()
+	ListItem_t *pindex;
+	ListItem_t listend;
+};
+typedef struct List List_t;
+void Listitem_init(ListItem_t *pitem)
 {
-	value=0;
+	pitem->value=0;
 }
-void List_init(List pxlist)
+void List_init(List_t *pxlist)
 {
-	pxlist.number=0;
-	pxlist->pre=NULL;
-	pxlist->next=NULL;
+	pxlist->number=0;
+	pxlist->pindex=(ListItem_t*)&pxlist->listend;
+	pxlist->listend.value=0xffff;
+	pxlist->listend.pre=(ListItem_t*)&pxlist->listend;
+	pxlist->listend.next=(ListItem_t*)&pxlist->listend;
+}
+void List_insert(List_t *pxlist,ListItem_t *newitem)
+{
+	if( !pxlist->number){
+
+	}
+	else{
+		
+	}
 }
 int main()
 {
+	List_t *mylist;
+	List_init(mylist);
+	
 	printf("hello\n");
 
 }
