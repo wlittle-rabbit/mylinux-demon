e8255a866d24052414f7b437366bc97c1a3ae65c
wlittle-rabbit <591276288@qq.com>
2019-10-26 20:05:38 +0800

    fix linklist.c,have not finish!

--- linklist.c
+++ linklist.c
@@ -3,8 +3,8 @@
 
 struct Listitem{
 	int value;
-	struct xListitem *pre;
-	struct xListitem *next;
+	struct Listitem *pre;
+	struct Listitem *next;
 }; 
 typedef struct Listitem ListItem_t;
 struct List{
@@ -21,41 +21,53 @@ void List_init(List_t *pxlist)
 {
 	pxlist->number=0;
 	pxlist->pindex=(ListItem_t*)&pxlist->listend;
-	pxlist->listend.value=0xffff;
+	pxlist->listend.value=0xff;
 	pxlist->listend.pre=(ListItem_t*)&pxlist->listend;
 	pxlist->listend.next=(ListItem_t*)&pxlist->listend;
 }
 void List_insert(List_t *pxlist,ListItem_t *newitem)
 {
-	pxlist->number++;
 	ListItem_t *p;
 	if( !pxlist->number){
 		p=pxlist->listend.pre;
+		pxlist->pindex=newitem;		
 	}
 	else{
-		for(p=(ListItem_t *)&pxlist->listend;p->value>newitem->value;p=p->pre);	
+		printf("pxlist->number is %d\n",pxlist->number);
+		for(p=(ListItem_t *)&pxlist->listend.pre;(p->value>=newitem->value)&&(p!=(ListItem_t *)&pxlist->listend.next);p=p->pre);	
 	}
 	newitem->next=p->next;	
-	newitem->next->pre=newitem;
+	newitem->next->pre = newitem;
 	p->next=newitem;
 	newitem->pre=p;
+	pxlist->number++;
+	printf("the newitem pre value is %d\n",newitem->pre->value);
+	printf("the newitem value is %d\n",newitem->value);
 }
 void list_print(List_t *pxlist)
 {
 	printf("the list number is %d\n",pxlist->number);
+	int i;
+	ListItem_t* tmp;
+	tmp=pxlist->pindex;
+	printf("the item value is %d\n",tmp->value);
+	for(tmp=pxlist->pindex;tmp!=(ListItem_t*)&pxlist->listend;tmp=tmp->next){
+		printf("the item value is %d\n",tmp->value);
+	}
 }
 int main()
 {
 	List_t *mylist;
+	mylist=malloc(10*sizeof(List_t));
 	List_init(mylist);
 	ListItem_t item1,item2,item3;
 	item1.value=1;
 	item2.value=2;
 	item3.value=3;
-	List_insert(mylist,&item2);
 	List_insert(mylist,&item3);
+	List_insert(mylist,&item2);
 	List_insert(mylist,&item1);
-
-	
-	printf("hello\n");
+	list_print(mylist);
+	free(mylist);
+	printf("main end\n");
 }
